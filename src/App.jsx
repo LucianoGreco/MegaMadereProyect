38ef35e79bb604584595f27ffa98ceab0dbc7ff9
LuchoGreco <grecolucianogabriel@gmail.com>
2024-11-27 03:15:43 -0300

    Actualizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// Estilos Globales
 import GlobalStyles from "./styles/globalStyles";
-import Home from "./pages/home.jsx";
-import Novedades from "./pages/novedades.jsx";
-import Simulador from "./pages/simuladorPage.jsx";
-import Envios from "./pages/envios.jsx";
-import Herrajes from "./pages/herrajes.jsx";
-import Melaminas from "./pages/melaminas.jsx";
-import Muebles from "./pages/muebles.jsx";
-import Contactos from "./pages/contactos.jsx";
-import Cookies from "./components/policies/Cookies.jsx";
+// Pages
+import Home       from "./pages/home.jsx";
+import Novedades  from "./pages/novedades.jsx";
+import Simulador  from "./pages/simuladorPage.jsx";
+import Envios     from "./pages/envios.jsx";
+import Herrajes   from "./pages/herrajes.jsx";
+import Melaminas  from "./pages/melaminas.jsx";
+import Muebles    from "./pages/muebles.jsx";
+import Contactos  from "./pages/contactos.jsx";
+// Compponents - Politicas
+import Cookies    from "./components/policies/Cookies.jsx";
 import Privacidad from "./components/policies/Privacidad.jsx";
-import Terminos from "./components/policies/Terminos.jsx";
-import Footer from "./components/layout/Footer.jsx";
-import Header from "./components/layout/Header.jsx";
-import Cursor from "./data/cursor.js"
+import Terminos   from "./components/policies/Terminos.jsx";
+// Componentes UI
+import Footer     from "./components/layout/Footer.jsx";
+import Header     from "./components/layout/Header.jsx";
+import Cursor     from "./data/cursor.js"
 
 const App = () => {
   return (
